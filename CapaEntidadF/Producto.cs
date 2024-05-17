15fe4d4d70ec6057755f6f3c9c816b71ba5b73ea
2529542023MC <2529542023@mail.utec.edu.sv>
2024-05-17 12:43:54 -0600

    Modulo Movimiento

---
+++ CapaEntidadF/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidadF
+{
+    public class Producto
+    {
+        public int id_producto { get; set; }
+        public string producto { get; set; }
+        public string descripcion { get; set; }
+        public double precio { get; set; }
+        public int tipo { get; set; }
+
+    }
+}
